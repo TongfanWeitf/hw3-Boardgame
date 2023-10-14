702eb84474fe2d950f35c88f1d670aca52b50204
魏 <weitf@bu.edu>
2023-10-13 20:22:39 -0400

    TODO: move validator and print

--- QMove.java
+++ QMove.java
@@ -4,6 +4,7 @@ public class QMove {
         //q is board of walls, 1 is horizontal, -1 is vertical, 0 is no wall.
         //in gameBoard, 0 is no piece, 1 is player 2 's piece (from top, odd Turns ), -1 is player 1 's piece (from bottom, even Turns)
         //Turns start from 0, not 1
+        //String m is L for left, R for right, or F for forward(left and right are based on players' side)
         //move the piece if legal, print a warning and jump the turn is illegal
         //only the piece move, not the wall move
     }
