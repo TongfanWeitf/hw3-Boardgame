9619e6729c36fc1196d795c716a2a3cacbf328a1
魏 <weitf@bu.edu>
2023-10-13 20:19:24 -0400

    TODO: move validator and print

---
+++ PrintBoard.java
@@ -0,0 +1,7 @@
+public class PrintBoard {
+    public static void print(gameBoard g, gameBoard q){
+        // TODO: print
+        //q is the Board of Wall. 1 is horizontal, -1 is vertical, 0 is no wall.
+        //qBoard.Board[0][0] is the wall that centered at the lower left corner of gameBoard.Board[0][0]
+    }
+}
---
+++ QMove.java
@@ -0,0 +1,14 @@
+public class QMove {
+    public static void Move(gameBoard g, gameBoard q, String m, int Turns){
+        //TODO: validator
+        //q is board of walls, 1 is horizontal, -1 is vertical, 0 is no wall.
+        //in gameBoard, 0 is no piece, 1 is player 2 's piece (from top, odd Turns ), -1 is player 1 's piece (from bottom, even Turns)
+        //Turns start from 0, not 1
+        //move the piece if legal, print a warning and jump the turn is illegal
+        //only the piece move, not the wall move
+    }
+
+    public static void BuildWall(gameBoard q, int i, int j, int d){
+        //I will write this
+    }
+}
