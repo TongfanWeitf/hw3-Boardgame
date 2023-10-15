0660f65964a74bc7be85496d1863a9ac0ca339e6
JialongKe <79288715+JialongKe@users.noreply.github.com>
2023-10-15 19:20:22 -0400

    Add files via upload

--- QMove.java
+++ QMove.java
@@ -7,6 +7,164 @@ public class QMove {
         //String m is L for left, R for right, or F for forward(left and right are based on players' side)
         //move the piece if legal, print a warning and jump the turn is illegal
         //only the piece move, not the wall move
+
+        // get target piece
+        int targetPieceId = 0;
+        Cell targetCell = null;
+        if (Turns % 2 == 0) {
+            targetPieceId = -1;
+        } else {
+            targetPieceId = 1;
+        }
+
+        // get target piece current location
+        int curRow = -1;
+        int curCol = -1;
+        for (int i = 0; i < g.getSizei(); i++) {
+            for (int j = 0; j < g.getSizej(); j++) {
+                if (g.getBoard()[i][j].getId() == targetPieceId) {
+                    curRow = i;
+                    curCol = j;
+                    targetCell = g.getBoard()[i][j];
+                    break;
+                }
+            }
+        }
+
+        // get piece location
+        int oldRow = curRow;
+        int oldCol = curCol;
+        if (m.equals("L")) { // move left
+            if (targetPieceId == 1) { // from top
+                if (curCol == g.getSizej() - 1) {
+                    System.out.println("Illegal move!");
+                    return;
+                }
+                if (curRow == 0 && q.getBoard()[0][curCol].getId() == -1) {
+                    System.out.println("Illegal move!");
+                    return;
+                }
+                if (curRow == g.getSizei()-1 && q.getBoard()[curRow-1][curCol].getId() == -1) {
+                    System.out.println("Illegal move!");
+                    return;
+                }
+                if ((curRow != 0 && curRow != g.getSizei()-1) &&
+                        (q.getBoard()[curRow-1][curCol].getId() == -1 ||
+                        q.getBoard()[curRow][curCol].getId() == -1)) {
+                    System.out.println("Illegal move!");
+                    return;
+                }
+                curCol++;
+            } else { // from bottom
+                if (curCol == 0) {
+                    System.out.println("Illegal move!");
+                    return;
+                }
+                if (curRow == 0 && q.getBoard()[0][curCol-1].getId() == -1) {
+                    System.out.println("Illegal move!");
+                    return;
+                }
+                if (curRow == g.getSizei()-1 && q.getBoard()[curRow-1][curCol-1].getId() == -1) {
+                    System.out.println("Illegal move!");
+                    return;
+                }
+                if ((curRow != 0 && curRow != g.getSizei()-1) && (
+                        q.getBoard()[curRow-1][curCol-1].getId() == -1 ||
+                        q.getBoard()[curRow][curCol-1].getId() == -1)) {
+                    System.out.println("Illegal move!");
+                    return;
+                }
+                curCol--;
+            }
+        } else if (m.equals("R")) { // move right
+            if (targetPieceId == 1) { // from top
+                if (curCol == 0) {
+                    System.out.println("Illegal move!");
+                    return;
+                }
+                if (curRow == 0 && q.getBoard()[0][curCol-1].getId() == -1) {
+                    System.out.println("Illegal move!");
+                    return;
+                }
+                if (curRow == g.getSizei()-1 && q.getBoard()[curRow-1][curCol-1].getId() == -1) {
+                    System.out.println("Illegal move!");
+                    return;
+                }
+                if ((curRow != 0 && curRow != g.getSizei() - 1) &&
+                        (q.getBoard()[curRow-1][curCol-1].getId() == -1 ||
+                        q.getBoard()[curRow][curCol-1].getId() == -1)) {
+                    System.out.println("Illegal move!");
+                    return;
+                }
+                curCol--;
+            } else { // from bottom
+                if (curCol == g.getSizej() - 1) {
+                    System.out.println("Illegal move!");
+                    return;
+                }
+                if (curRow == 0 && q.getBoard()[0][curCol].getId() == -1) {
+                    System.out.println("Illegal move!");
+                    return;
+                }
+                if (curRow == g.getSizei()-1 && q.getBoard()[curRow-1][curCol].getId() == -1) {
+                    System.out.println("Illegal move!");
+                    return;
+                }
+                if ((curRow != 0 && curRow != g.getSizei() - 1) &&
+                        (q.getBoard()[curRow-1][curCol].getId() == -1 ||
+                        q.getBoard()[curRow][curCol].getId() == -1)) {
+                    System.out.println("Illegal move!");
+                    return;
+                }
+                curCol++;
+            }
+        } else { // move forward
+            if (targetPieceId == 1) { // from top
+                if (curRow == g.getSizei()-1) {
+                    System.out.println("Illegal move!");
+                    return;
+                }
+                if (curCol == 0 && q.getBoard()[curRow][0].getId() == 1) {
+                    System.out.println("Illegal move!");
+                    return;
+                }
+                if (curCol == g.getSizej()-1 && q.getBoard()[curRow][curCol-1].getId() == 1) {
+                    System.out.println("Illegal move!");
+                    return;
+                }
+                if ((curCol != 0 && curCol != g.getSizej() - 1) &&
+                        (q.getBoard()[curRow][curCol-1].getId() == 1 ||
+                        q.getBoard()[curRow][curCol].getId() == 1)) {
+                    System.out.println("Illegal move!");
+                    return;
+                }
+                curRow++;
+            } else { // from bottom
+                if (curRow == 0) {
+                    System.out.println("Illegal move!");
+                    return;
+                }
+                if (curCol == 0 && q.getBoard()[curRow-1][0].getId() == 1) {
+                    System.out.println("Illegal move!");
+                    return;
+                }
+                if (curCol == g.getSizej()-1 && q.getBoard()[curRow-1][curCol-1].getId() == 1) {
+                    System.out.println("Illegal move!");
+                    return;
+                }
+                if ((curCol != 0 && curCol != g.getSizej() - 1) &&
+                        (q.getBoard()[curRow-1][curCol-1].getId() == 1 ||
+                        q.getBoard()[curRow-1][curCol].getId() == 1)) {
+                    System.out.println("Illegal move!");
+                    return;
+                }
+                curRow--;
+            }
+        }
+
+        // check move is valid or not
+        g.Move(curRow, curCol, targetCell);
+        g.getBoard()[oldRow][oldCol] = new TTTPiece(0);
     }
 
     public static void BuildWall(gameBoard q, int i, int j, TTTPiece p){
